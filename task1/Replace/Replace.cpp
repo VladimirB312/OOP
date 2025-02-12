b5217be992b8cf4c88e2f1c040bba4ca765fc4f1
VladimirB312 <158708298+VladimirB312@users.noreply.github.com>
2025-02-12 22:58:44 +0300

    добавил ввод через stdin, тесты

--- task1/Replace/Replace.cpp
+++ task1/Replace/Replace.cpp
@@ -1,6 +1,9 @@
-#include "stdafx.h"
 #include <string>
 #include <optional>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 struct Args
 {
@@ -10,7 +13,7 @@ struct Args
 	std::string replacementString;
 };
 
-std::optional<Args> getParams(const int argc, char* argv[])
+std::optional<Args> GetParams(const int argc, char* argv[])
 {
 	if (argc != 5)
 	{
@@ -29,9 +32,13 @@ std::optional<Args> getParams(const int argc, char* argv[])
 std::string ReplaceString(const std::string& subject,
 	const std::string& searchString, const std::string& replacementString)
 {
+	if (searchString.size() == 0)
+	{
+		return subject;
+	}
+
 	size_t pos = 0;
 	std::string result;
-
 	while (pos < subject.size())
 	{
 		size_t foundPos = subject.find(searchString, pos);
@@ -50,6 +57,7 @@ std::string ReplaceString(const std::string& subject,
 void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
 	const std::string& searchString, const std::string& replacementString)
 {
+
 	std::string line;
 
 	while (std::getline(input, line))
@@ -58,52 +66,123 @@ void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
 	}
 }
 
-void CopyFileWithReplacement(const std::optional<Args> args, bool& isCopySuccess)
+void CopyFileWithReplacement(const Args& args, bool& isCopySuccess)
 {
 	std::ifstream inputFile;
 	std::ofstream outputFile;
-	inputFile.open(args->inputFileName);
-	outputFile.open(args->outputFileName);
+	inputFile.open(args.inputFileName);
+	outputFile.open(args.outputFileName);
 
 	if (!inputFile.is_open())
 	{
-		std::cout << "Failed to open input file: " << args->inputFileName << "\n";
+		std::cout << "Failed to open input file: " << args.inputFileName << "\n";
 		isCopySuccess = false;
 		return;
 	}
 
 	if (!outputFile.is_open())
 	{
-		std::cout << "Failed to open output file: " << args->outputFileName << "\n";
+		std::cout << "Failed to open output file: " << args.outputFileName << "\n";
 		isCopySuccess = false;
 		return;
 	}
 
-	CopyStreamWithReplacement(inputFile, outputFile, args->searchString, args->replacementString);
-	if (!outputFile.flush()) 
+	CopyStreamWithReplacement(inputFile, outputFile, args.searchString, args.replacementString);
+	if (!outputFile.flush())
 	{
 		std::cout << "Failed to save data on disk\n";
 		isCopySuccess = false;
 	}
 }
 
-int main(int argc, char* argv[])
+void ReplaceWordFromFile(const int argc, char* argv[], bool& isCopySuccess)
 {
-	const std::optional<Args> args = getParams(argc, argv);
+	const std::optional<Args> args = GetParams(argc, argv);
 
 	if (!args)
 	{
 		std::cout << "Invalid argument count\n"
 			<< "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		isCopySuccess = false;
+
+		return;
+	}
+
+	CopyFileWithReplacement(args.value(), isCopySuccess);
+}
+
+std::string LinesToString()
+{
+	std::vector<std::string> inputStrings;
+	std::string line;
+	while (std::getline(std::cin, line))
+	{
+		inputStrings.push_back(line);
+	}
+
+	std::string allLines;
+	for (const auto& str : inputStrings)
+	{
+		allLines.append(str).append("\n");
+	}
+
+	return allLines;
+}
+
+void ReplaceWordFromStdin()
+{
+	std::string searchString, replacementString;
+
+	std::getline(std::cin, searchString);
+	if (std::cin.eof())
+	{
+		std::cout << "ERROR\n";
+		return;
+	}
+
+	std::getline(std::cin, replacementString);
+	if (std::cin.eof())
+	{
+		std::cout << "ERROR\n";
+		return;
+	}
+
+	std::istringstream inputStream(LinesToString());
+	std::ostringstream outputStream;
+
+	CopyStreamWithReplacement(inputStream, outputStream, searchString, replacementString);
+	std::cout << outputStream.str();
+}
+
+int main(int argc, char* argv[])
+{
+	bool isCopySuccess = true;
+	const std::string HELP_ARG = "-h";
+
+	switch (argc)
+	{
+	case 1:
+		ReplaceWordFromStdin();
+		break;
+	case 2:
+	{
+		if (argv[1] != HELP_ARG)
+		{
+			std::cout << "ERROR\n";
+			return 1;
+		}
+		std::cout << "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		break;
+	}
+	case 5:
+		ReplaceWordFromFile(argc, argv, isCopySuccess);
+		break;
+	default:
 		return 1;
 	}
 
-	bool isCopySuccess = true;
-	CopyFileWithReplacement(args, isCopySuccess);
-
 	if (!isCopySuccess)
 	{
-		std::cout << "Failed to save data on disk\n";
 		return 1;
 	}
 
--- task1/Replace/test.bat
+++ task1/Replace/test.bat
@@ -4,31 +4,51 @@ rem %~1 - значение первого аргумента командной строки bat-файла с удалением обра
 rem ѕеременна€ PROGRAM будет хранить первый аргумент командной строки заключЄнный в кавычки
 set PROGRAM="%~1"
 
-rem ѕри запуске без параметров ожидаетс€ ненулевой код возврата
-%PROGRAM% > nul
+rem ѕри запуске с одним параметром отличным от -h ожидаетс€ ненулевой код возврата
+%PROGRAM% -v >nul
 if NOT ERRORLEVEL 1 goto err
 echo Test 1 passed
 
-rem ѕри запуске с правильными параметрами ожидаетс€ нулевой код возврата  добавить описание теста 
+rem ѕри запуске с правильными параметрами ожидаетс€ нулевой код возврата
+rem ¬ случае отсутстви€ искомой строки во входном файле, выходной файл должен быть равен входному
 %PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" bird cat
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\fox.txt" test-data\fox.txt >nul
 if ERRORLEVEL 1 goto err
 echo Test 2 passed
 
-rem ѕри запуске с правильными параметрами ожидаетс€ нулевой код возврата
+rem “ест замены во входном файле строки "dog" на "cat"
 %PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" dog cat
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\fox.txt" test-data\fox-replace-dog-with-cat.txt >nul
 if ERRORLEVEL 1 goto err
 echo Test 3 passed
 
-rem ѕри запуске с правильными параметрами ожидаетс€ нулевой код возврата
+rem “ест замены во входном файле строки "1231234" на "XYZ"
 %PROGRAM% test-data\digits.txt "%TEMP%\digits.txt" 1231234 XYZ
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\digits.txt" test-data\digits-replace-1231234-with-XYZ.txt >nul
 if ERRORLEVEL 1 goto err
 echo Test 4 passed
+
+rem “ест замены во входном файле, состо€щим из нескольких строк, строки "brown" на "red"
+%PROGRAM% test-data\multiline.txt "%TEMP%\multiline.txt" brown red
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\multiline.txt" test-data\multiline-replace.txt >nul
+if ERRORLEVEL 1 goto err
+echo Test 5 passed
+
+rem “ест замены в с многократным вхождением искомой строки в строку-заменитель "ma" "mama"
+%PROGRAM% test-data\mama.txt "%TEMP%\mama.txt" ma mama
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\mama.txt" test-data\mama-replace.txt >nul
+if ERRORLEVEL 1 goto err
+echo Test 6 passed
+
+rem “ест отсутстви€, заданного параметрами, входного файла, должен вернуть ненулевой код возврата
+%PROGRAM% test-data\missing.txt "%TEMP%\mama.txt" ma mama >nul
+if NOT ERRORLEVEL 1 goto err
+echo Test 7 passed
 
 echo OK
 exit 0
